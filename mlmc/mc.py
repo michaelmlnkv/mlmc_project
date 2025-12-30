c785de6d4a1ef28e16cac036a2e36b3e02ca9fad
Mike <michaelmelnikov27@gmail.com>
2025-12-30 17:05:35 +0100

    finished asian + barrier pricing, created convergence files

--- mlmc/mc.py
+++ mlmc/mc.py
@@ -5,11 +5,38 @@ from sde import simulate_gbm_paths_recursive
 from mlmc.payoffs import asian_payoff_per_path
 
 @njit
-def asian_price_mc(paths, strike_price, r, T):
+def _mean_and_var(payoffs_p_path):
+    mean = np.mean(payoffs_p_path)
+    var = np.var(payoffs_p_path)
+    return mean, var
+@njit
+def _inner_asian_price_mc(paths, strike_price, r, T):
     payoffs = asian_payoff_per_path(paths, strike_price)
-    return np.exp(-r*T) * np.mean(payoffs)
+    num_paths = np.shape(payoffs)[0]
+    mean, var = _mean_and_var(payoffs)
+    mean *= np.exp(-r*T)
+    se = np.sqrt(var / num_paths)
+    return mean, se
 
 @njit
-def barrier_price_mc(paths, strike_price, barrier, r, T):
+def _inner_barrier_price_mc(paths, strike_price, barrier, r, T):
     payoffs = barrier_payoff_per_path(paths, strike_price, barrier)
-    return np.exp(-r*T) * np.mean(payoffs)
+    num_paths = np.shape(payoffs)[0]
+    mean, var = _mean_and_var(payoffs)
+    mean *= np.exp(-r * T)
+    se = np.sqrt(var / num_paths)
+    return mean, se
+
+def asian_price_mc(S0, mu, sigma, n_steps, n_paths, strike_price, r, T):
+    paths = simulate_gbm_paths_recursive(S0, mu, sigma, T, n_steps, n_paths)
+    mean, se = _inner_asian_price_mc(paths, strike_price, r, T)
+    return mean, se
+
+def barrier_price_mc(S0, mu, sigma, n_steps, n_paths, strike_price, barrier, r, T):
+    paths = simulate_gbm_paths_recursive(S0, mu, sigma, T, n_steps, n_paths)
+    mean, se = _inner_barrier_price_mc(paths, strike_price, barrier, r, T)
+    return mean, se
+
+S0, r, sigma, T, K = 100.0, 0.05, 0.2, 1.0, 100.0
+price, se = asian_price_mc(S0, r, sigma, 128, 50000, K, r, T)
+print(price, se)
